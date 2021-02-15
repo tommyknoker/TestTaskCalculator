eec61d0bc4271a95b36a71946e2067b9939d5652
tommyknoker <eremin_fedor@mail.ru>
2021-02-15 14:10:17 +0300

    Спецификатор "%d" предназначен для вывода целочисленных значений. исправил: добавил проверку если дробная часть равна 0 то приводим переменную "x" к типу "int" и выводим на экран, в противном случае выводим на экран переменную типа "double"

--- src/OutputData.java
+++ src/OutputData.java
@@ -1,3 +1,5 @@
+import java.text.DecimalFormat;
+
 public class OutputData {
 
     void OutputData(String[] array, String NumSys){
@@ -16,8 +18,12 @@ public class OutputData {
         if (NumSys == "Arab"){
             if (array[1].equals("/")) {
                 double x = new CalcArab(" ", Integer.parseInt(array[0]), Integer.parseInt(array[2])).GetDivRes();
-                System.out.printf("Результат: \n%d",x);
-
+                if(x % 1 == 0) {
+                    System.out.println("Результат: \n"+(int)x);
+                }
+                else {
+                    System.out.println("Результат: \n"+x);
+                }
             }
             else {
                 int x = new CalcArab(array[1], Integer.parseInt(array[0]), Integer.parseInt(array[2])).GetRes();
